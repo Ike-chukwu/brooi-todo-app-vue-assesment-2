023b646598581f4a548db6b3c551b5b13d07d5f9
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2024-02-12 21:23:51 +0100

    quicl fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/brooi-todo-app-vue-assesment-2/" : "/",
-})
+  publicPath: "/brooi-todo-app-vue-assesment-2/",
+});
